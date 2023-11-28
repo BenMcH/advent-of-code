b7f86eb730aa1ecc5f7af66401755f676d1bb7de
Ben McHone <ben@mchone.dev>
2023-11-28 00:34:32 +0000

    2019 day 5 part 1

--- 2019/day_05/main_test.go
+++ 2019/day_05/main_test.go
@@ -2,6 +2,8 @@ package day_05
 
 import (
 	"advent-of-code-2019/intcode"
+	"advent-of-code-2019/utils"
+	"fmt"
 	"testing"
 )
 
@@ -17,34 +19,51 @@ func PartOne(lower, upper int) int {
 	return 0
 }
 
-// func TestImmediateMultiplication(t *testing.T) {
-// 	intcode.ExecuteIntcodeFromString("1002,4,3,4,33")
-// 	fmt.Println("Here")
-// 	intcode.ExecuteIntcodeFromString("102,3,4,4,33")
-// }
-// func TestPartOne(t *testing.T) {
-// 	input := utils.ReadInput(5)
+func TestImmediateMultiplication(t *testing.T) {
+	program, output := intcode.ExecuteIntcodeFromString("1002,4,3,4,33")
 
-// 	_, output := intcode.ExecuteIntcodeFromString(input)
+	if program[4] != 99 {
+		t.Errorf("Expected program[4] to be 99. Got %d", program[4])
+	}
 
-// 	for i := 0; i < len(output)-1; i++ {
-// 		if output[i] != 0 {
-// 			t.Errorf("Expected test to pass; Got: %d at index %d", output[i], i)
-// 		}
-// 	}
+	if len(output) != 0 {
+		t.Errorf("Did not expect any output. Got %v", output)
+	}
+	
+	program, output = intcode.ExecuteIntcodeFromString("102,33,4,4,3")
 
-// 	// min, max := ParseInput(input)
+	if program[4] != 99 {
+		t.Errorf("Expected program[4] to be 99. Got %d", program[4])
+	}
 
-// 	fmt.Printf("Day 5 Part 1: %v\n", output)
-// }
+	if len(output) != 0 {
+		t.Errorf("Did not expect any output. Got %v", output)
+	}
+}
+func TestPartOne(t *testing.T) {
+	input := utils.ReadInput(5)
 
-func PartTwo(lower, upper int) int {
-	count := 0
-	return count
+	_, output := intcode.ExecuteIntcodeFromString(input)
+
+	for i := 0; i < len(output)-1; i++ {
+		if output[i] != 0 {
+			fmt.Println(output)
+			t.Errorf("Expected test to pass; Got: %d at index %d", output[i], i)
+		}
+	}
+
+	// min, max := ParseInput(input)
+
+	fmt.Printf("Day 5 Part 1: %v\n", output[len(output)-1])
 }
 
-func TestPartTwo(t *testing.T) {
-	// input := utils.ReadInput(5)
+// func PartTwo(lower, upper int) int {
+// 	count := 0
+// 	return count
+// }
 
-	// fmt.Printf("Day 4 Part 2: %d\n", PartTwo(1, 0))
-}
+// func TestPartTwo(t *testing.T) {
+// 	// input := utils.ReadInput(5)
+
+// 	// fmt.Printf("Day 4 Part 2: %d\n", PartTwo(1, 0))
+// }
--- 2019/intcode/intcode.go
+++ 2019/intcode/intcode.go
@@ -7,16 +7,47 @@ import (
 )
 
 const (
+	/*
+	Opcode 1 adds together numbers read from two positions and stores the result in a third position.
+	The three integers immediately after the opcode tell you these three positions
+	- the first two indicate the positions from which you should read the input values
+	- the third indicates the position at which the output should be stored.
+	*/
 	ADD            = 1
+	/*
+	Opcode 2 works exactly like opcode 1, except it multiplies the two inputs instead of adding them.
+	Again, the three integers after the opcode indicate where the inputs and outputs are, not their values.
+	*/
 	MULTIPLY       = 2
+	/*
+	Opcode 3 takes a single integer as input and saves it to the position given by its only parameter.
+	For example, the instruction 3,50 would take an input value and store it at address 50.
+	*/
 	INPUT          = 3
+	/*
+	Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50.
+	*/
 	OUTPUT         = 4
 	END_OF_PROGRAM = 99
 )
 
-func ExecuteIntcodeFromString(intcode string) ([]int, []int) {
-	program, _ := utils.StringsToInts(strings.Split(intcode, ","))
-	return ExecuteIntcode(program)
+/*
+Parameter modes
+
+0 - Default. position mode == causes the parameter to be interpreted as a position - if the parameter is 50, its value is the value stored at address 50 in memory.
+1 - Immediate Mode == In immediate mode, a parameter is interpreted as a value - if the parameter is 50, its value is simply 50.
+
+Parameter modes are stored in the same value as the instruction's opcode.
+The opcode is a two-digit number based only on the ones and tens digit of the value, that is, the opcode is the rightmost two digits of the first value in an instruction.
+Parameter modes are single digits, one per parameter, read right-to-left from the opcode: the first parameter's mode is in the hundreds digit, the second parameter's mode is in the thousands digit,
+the third parameter's mode is in the ten-thousands digit, and so on. Any missing modes are 0.
+
+Parameters that an instruction writes to will never be in immediate mode.
+*/
+
+func ExecuteIntcodeFromString(intcode string) (program []int, output []int) {
+	prg, _ := utils.StringsToInts(strings.Split(intcode, ","))
+	return ExecuteIntcode(prg)
 }
 
 func ExecuteIntcode(intcode []int) (program []int, output []int) {
@@ -29,47 +60,35 @@ func ExecuteIntcode(intcode []int) (program []int, output []int) {
 	for pos < len(program) {
 		memoryValue := program[pos]
 		opcode := memoryValue % 100
-		memoryValue -= opcode
-		memoryValue /= 100
-
-		parameterModes := []int{}
-
-		for memoryValue != 0 {
-			parameterMode := memoryValue % 10
-			parameterModes = append(parameterModes, parameterMode)
-			memoryValue = (memoryValue - parameterMode) / 10
-		}
-
-		fmt.Printf("Opcode: %d, modes: %v, pos: %d\n", opcode, parameterModes, pos)
+		parameterModes :=  (memoryValue - opcode) / 100
 
 		switch opcode {
 		case ADD:
 			args := program[pos : pos+4]
 			a, b, nPos := args[1], args[2], args[3]
-			fmt.Println(args[1:], a, b)
-			if len(parameterModes) == 0 || parameterModes[0] == 0 {
+			mode := parameterModes % 10
+			parameterModes /= 10
+			if mode == 0 {
 				a = program[a]
-				parameterModes = utils.Pop(parameterModes)
 			}
-			if len(parameterModes) == 0 || parameterModes[0] == 0 {
+			mode = parameterModes % 10
+			parameterModes /= 10
+			if mode == 0 {
 				b = program[b]
-				// parameterModes = utils.Pop(parameterModes)
 			}
-			// fmt.Println(args[1:], a, b)
-			// if len(parameterModes) == 0 || parameterModes[0] == 0 {
-			// 	nPos = program[nPos]
-			// }
-			// a, b, nPos = program[args[1]], program[args[2]], args[3]
 			program[nPos] = a + b
 			pos += 4
 		case MULTIPLY:
 			args := program[pos : pos+4]
 			a, b, nPos := args[1], args[2], args[3]
-			if len(parameterModes) == 0 || parameterModes[0] == 0 {
+			mode := parameterModes % 10
+			parameterModes /= 10
+			if mode == 0 {
 				a = program[a]
-				parameterModes = utils.Pop(parameterModes)
 			}
-			if len(parameterModes) == 0 || parameterModes[0] == 0 {
+			mode = parameterModes % 10
+			parameterModes /= 10
+			if mode == 0 {
 				b = program[b]
 			}
 			program[nPos] = a * b
@@ -83,8 +102,10 @@ func ExecuteIntcode(intcode []int) (program []int, output []int) {
 		case OUTPUT:
 			param := program[pos+1]
 			value := param
+			mode := parameterModes % 10
+			parameterModes /= 10
 
-			if len(parameterModes) == 0 || parameterModes[0] == 0 {
+			if mode == 0 {
 				value = program[value]
 			}
 			pos += 2
