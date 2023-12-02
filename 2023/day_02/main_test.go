ca166a5a189f404943dc5ec494e944276b6f318c
Ben McHone <ben@mchone.dev>
2023-12-02 05:40:25 +0000

    Part 1

--- 2023/day_02/main_test.go
+++ 2023/day_02/main_test.go
@@ -1,15 +1,68 @@
 package day02
 
-import "testing"
+import (
+	"advent-of-code-2023/utils"
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+)
 
-func TestFunc() string {
-	return "Hello world"
+type Game struct {
+	blue, red, green int
 }
 
-func TestTestFunc(t *testing.T) {
-	got := TestFunc()
+func ParseGame(rounds []string) Game {
+	g := Game{}
+	for _, round := range rounds {
+		moves := strings.Split(strings.TrimLeft(round, " "), ", ")
+		for _, move := range moves {
+			vals := strings.Split(move, " ")
+			num, _ := strconv.Atoi(vals[0])
 
-	if got != "Hello World!"{
-		t.Errorf("Got: %s, expected: \"Hello World!\"", got)
+			if strings.HasPrefix(vals[1], "blue") {
+				g.blue = num
+			} else if strings.HasPrefix(vals[1], "red") {
+				g.red = num
+			} else if strings.HasPrefix(vals[1], "green") {
+				g.green = num
+			} else {
+				fmt.Println("No idea", vals)
+			}
+
+			if !g.PartOnePossible() {
+				return g
+			}
+		}
 	}
+	return g
 }
+
+func (g Game) PartOnePossible() bool {
+	return g.red <= 12 && g.green <= 13 && g.blue <= 14
+}
+
+func TestPartOne(t *testing.T) {
+	input := utils.ReadInput(2)
+	games := strings.Split(input, "\n")
+
+	sum := 0
+	for _, game := range games {
+		sections := strings.Split(game, ":")
+		id, _ := strconv.Atoi(strings.Split(sections[0], " ")[1])
+		rounds := strings.Split(sections[1], "; ")
+		game := ParseGame(rounds)
+		fmt.Println(game)
+
+		if game.PartOnePossible() {
+			sum += id
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+// func TestPartTwo(t *testing.T) {
+// 	input := utils.ReadInput(2)
+// 	fmt.Println(input)
+// }
