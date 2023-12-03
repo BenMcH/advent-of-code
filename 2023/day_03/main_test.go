cf7a672cb425b95c563afc5572d5d942bc20b073
Ben McHone <ben@mchone.dev>
2023-12-03 06:53:52 +0000

    Part 2

--- 2023/day_03/main_test.go
+++ 2023/day_03/main_test.go
@@ -28,58 +28,72 @@ func ExtractNumberAt(board [][]rune, x, y int) string {
 	return string(runes)
 }
 
+type Element struct {
+	value    rune
+	row, col int
+}
+
+func SurroundingElements(board [][]rune, col, row int) []Element {
+	arr := make([]Element, 0)
+	up := row > 0
+	down := row < len(board)-1
+	left := col > 0
+	right := col < len(board[row])-1
+	if up {
+		arr = append(arr, Element{board[row-1][col], row - 1, col})
+		if left {
+			arr = append(arr, Element{board[row-1][col-1], row - 1, col - 1})
+		}
+		if right {
+			arr = append(arr, Element{board[row-1][col+1], row - 1, col + 1})
+		}
+	}
+	if left {
+		arr = append(arr, Element{board[row][col-1], row, col - 1})
+	}
+	if right {
+		arr = append(arr, Element{board[row][col+1], row, col + 1})
+	}
+	if down {
+		arr = append(arr, Element{board[row+1][col], row + 1, col})
+		if left {
+			arr = append(arr, Element{board[row+1][col-1], row + 1, col - 1})
+		}
+		if right {
+			arr = append(arr, Element{board[row+1][col+1], row + 1, col + 1})
+		}
+	}
+
+	return arr
+}
+
 func IsSymbol(input rune) bool {
-	// symbols := "!@#$%^&*()/\\+=-"
 	sym := input != '.' && !unicode.IsDigit(input) && input != '\n'
-	// if sym && !strings.ContainsRune(symbols, input) {
-	// 	fmt.Printf("Symbol %v\n", strconv.QuoteRune(input))
-	// }
 	return sym
 }
 
-func FindPartNumbers(board [][]rune) []int {
-	partNumbers := make([]int, 0)
+type PartNumber struct {
+	number   int
+	row, col int
+}
+
+func FindPartNumbers(board [][]rune) []PartNumber {
+	partNumbers := make([]PartNumber, 0)
 
 	for row := 0; row < len(board); row++ {
 		line := board[row]
-		up := row > 0
-		down := row < len(board)-1
 		for col := 0; col < len(line); col++ {
-			if row == 7 && col == 7 {
-				fmt.Println("lastrow")
-			}
 			char := line[col]
-			left := col > 0
-			right := col < len(board[row])-1
-			if row == 0 && col == 2 {
-				fmt.Println(string(char), up, down, left, right)
-			}
 			isSurrounded := false
 
 			if !unicode.IsNumber(char) {
 				continue
 			}
-			if up {
-				if IsSymbol(board[row-1][col]) {
-					isSurrounded = true
-				} else if left && IsSymbol(board[row-1][col-1]) {
-					isSurrounded = true
-				} else if right && IsSymbol(board[row-1][col+1]) {
-					isSurrounded = true
-				}
-			}
-			if left && IsSymbol(line[col-1]) {
-				isSurrounded = true
-			}
-			if right && IsSymbol(line[col+1]) {
-				isSurrounded = true
-			}
-			if down {
-				if IsSymbol(board[row+1][col]) {
-					isSurrounded = true
-				} else if left && IsSymbol(board[row+1][col-1]) {
-					isSurrounded = true
-				} else if right && IsSymbol(board[row+1][col+1]) {
+
+			surrounding := SurroundingElements(board, col, row)
+
+			for _, val := range surrounding {
+				if IsSymbol(val.value) {
 					isSurrounded = true
 				}
 			}
@@ -88,7 +102,7 @@ func FindPartNumbers(board [][]rune) []int {
 				numberStr := ExtractNumberAt(board, col, row)
 				number, _ := strconv.Atoi(numberStr)
 
-				partNumbers = append(partNumbers, number)
+				partNumbers = append(partNumbers, PartNumber{number, row, col})
 
 				for col < len(line)-1 && unicode.IsDigit(line[col+1]) {
 					col += 1
@@ -102,7 +116,6 @@ func FindPartNumbers(board [][]rune) []int {
 
 func PartOne(input string) int {
 	lines := strings.Split(strings.Trim(input, "\n"), "\n")
-	fmt.Println(strconv.Quote(lines[0]))
 	characters := make([][]rune, len(lines))
 
 	for row, line := range lines {
@@ -117,7 +130,7 @@ func PartOne(input string) int {
 	sum := 0
 
 	for _, num := range parts {
-		sum += num
+		sum += num.number
 	}
 
 	return sum
@@ -140,4 +153,71 @@ func TestPartOne(t *testing.T) {
 	fmt.Println(PartOne(testInput))
 	input := utils.ReadInput(3)
 	fmt.Println(PartOne(input))
+}
+
+func PartTwo(input string) int {
+	lines := strings.Split(strings.Trim(input, "\n"), "\n")
+	characters := make([][]rune, len(lines))
+
+	for row, line := range lines {
+		characters[row] = make([]rune, len(line))
+		for col, char := range line {
+			characters[row][col] = char
+		}
+	}
+
+	sum := 0
+
+	for row, line := range characters {
+		for col, char := range line {
+			if char != '*' {
+				continue
+			}
+
+			surrounding := SurroundingElements(characters, col, row)
+
+			numbers := make(map[int]bool)
+
+			for _, val := range surrounding {
+				if !unicode.IsNumber(val.value) {
+					continue
+				}
+				num := ExtractNumberAt(characters, val.col, val.row)
+				number, _ := strconv.Atoi(num)
+				numbers[number] = true
+			}
+
+			if len(numbers) == 2 {
+				ratio := 1
+				for key := range numbers {
+					ratio *= key
+				}
+
+				sum += ratio
+			}
+
+		}
+	}
+
+	return sum
+}
+
+func TestPartTwo(t *testing.T) {
+	testInput := `
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+`
+
+	fmt.Println(PartTwo(testInput))
+
+	input := utils.ReadInput(3)
+	fmt.Println(PartTwo(input))
 }
