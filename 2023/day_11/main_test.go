07c06ffedf568077d14876f27b7ab95689f2c69a
Ben McHone <ben@mchone.dev>
2023-12-11 05:54:52 +0000

    Part 2; Made a generic function to allow any amount of galaxy expansion in O(n) time

--- 2023/day_11/main_test.go
+++ 2023/day_11/main_test.go
@@ -6,37 +6,33 @@ import (
 	"testing"
 )
 
-func expandSpaceRows(input [][]rune) [][]rune {
-	newSpace := make([][]rune, 0)
-
-	for _, row := range input {
-		newSpace = append(newSpace, row)
-		if utils.All(row, func(r rune) bool { return r == '.' }) {
-			newSpace = append(newSpace, row)
-		}
-	}
-
-	return newSpace
-}
-
-func partOne(input string) int {
+func galacticDistances(input string, offset int) int {
 	lines := utils.Lines(input)
 
 	gal := utils.Map(lines, func(input string, idx int) []rune { return []rune(input) })
 
-	gal = expandSpaceRows(gal)
-	gal = utils.Transpose(gal)
-	gal = expandSpaceRows(gal)
-	gal = utils.Transpose(gal)
-
 	cells := make([]utils.Point, 0)
 
-	for y, line := range gal {
-		for x, char := range line {
-			if char == '#' {
-				cells = append(cells, utils.Point{x, y})
-			}
+	symbolicY, symbolicX := 0, 0
+	for _, line := range gal {
+		if utils.All(line, func(r rune) bool { return r == '.' }) {
+			symbolicY = symbolicY + offset
+			continue
 		}
+
+		symbolicX = 0
+		for x, pt := range line {
+			if utils.All(gal, func(r []rune) bool { return r[x] == '.' }) {
+				symbolicX = symbolicX + offset
+				continue
+			}
+
+			if pt == '#' {
+				cells = append(cells, utils.Point{symbolicX, symbolicY})
+			}
+			symbolicX++
+		}
+		symbolicY++
 	}
 
 	sum := 0
@@ -51,7 +47,7 @@ func partOne(input string) int {
 }
 
 func TestPartOne(t *testing.T) {
-	got := partOne(`...#......
+	got := galacticDistances(`...#......
 .......#..
 #.........
 ..........
@@ -61,11 +57,31 @@ func TestPartOne(t *testing.T) {
 ..........
 .......#..
 #...#.....
-`)
+`, 2)
 
 	if got != 374 {
 		t.Error("Wrong", got)
 	} else {
-		fmt.Println(partOne(utils.ReadInput(11)))
+		fmt.Println(galacticDistances(utils.ReadInput(11), 2))
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	got := galacticDistances(`...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....
+`, 100)
+
+	if got != 8410 {
+		t.Error("Wrong", got)
+	} else {
+		fmt.Println(galacticDistances(utils.ReadInput(11), 1000000))
 	}
 }
