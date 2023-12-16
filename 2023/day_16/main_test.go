b492dd6d2aa7c8a3c364afd98ab102d549564874
Ben McHone <ben@mchone.dev>
2023-12-16 22:49:06 +0000

    Struct approach

--- 2023/day_16/main_test.go
+++ 2023/day_16/main_test.go
@@ -28,98 +28,130 @@ type move struct {
 	loc, dir utils.Point
 }
 
-func FindEnergyLevel(grid utils.Grid, loc, dir utils.Point, beams map[utils.Point]int, moves map[move]int) int {
-	mv := move{loc, dir}
-	if _, ok := moves[mv]; ok {
-		return 0
-	}
-	moves[mv]++
-	if loc.X < grid.MinX || loc.X > grid.MaxX || loc.Y < grid.MinY || loc.Y > grid.MaxY {
-		return 0
-	}
-
-	beams[loc]++
-	cell := grid.Data[loc]
-	newLoc := loc.Add(dir)
-
-	if cell == '.' {
-		FindEnergyLevel(grid, newLoc, dir, beams, moves)
-	} else if cell == '|' {
-		if dir == RIGHT || dir == LEFT {
-			FindEnergyLevel(grid, loc.Up(), UP, beams, moves)
-			FindEnergyLevel(grid, loc.Down(), DOWN, beams, moves)
-		} else {
-			FindEnergyLevel(grid, newLoc, dir, beams, moves)
-		}
-	} else if cell == '-' {
-		if dir == UP || dir == DOWN {
-			FindEnergyLevel(grid, loc.Left(), LEFT, beams, moves)
-			FindEnergyLevel(grid, loc.Right(), RIGHT, beams, moves)
-		} else {
-			FindEnergyLevel(grid, newLoc, dir, beams, moves)
-		}
-	} else if cell == '/' {
-		if dir == UP {
-			FindEnergyLevel(grid, loc.Right(), RIGHT, beams, moves)
-		}
-		if dir == DOWN {
-			FindEnergyLevel(grid, loc.Left(), LEFT, beams, moves)
-		}
-		if dir == LEFT {
-			FindEnergyLevel(grid, loc.Down(), DOWN, beams, moves)
-		}
-		if dir == RIGHT {
-			FindEnergyLevel(grid, loc.Up(), UP, beams, moves)
-		}
-	} else if cell == '\\' {
-		if dir == DOWN {
-			FindEnergyLevel(grid, loc.Right(), RIGHT, beams, moves)
-		}
-		if dir == UP {
-			FindEnergyLevel(grid, loc.Left(), LEFT, beams, moves)
-		}
-		if dir == LEFT {
-			FindEnergyLevel(grid, loc.Up(), UP, beams, moves)
-		}
-		if dir == RIGHT {
-			FindEnergyLevel(grid, loc.Down(), DOWN, beams, moves)
-		}
-	}
-
-	return len(beams)
+type MirrorMaze struct {
+	grid  utils.Grid
+	cache map[move]map[utils.Point]int
 }
 
+func (m MirrorMaze) FindBeams(loc, dir utils.Point, beams map[utils.Point]int) map[utils.Point]int {
+	mv := move{loc, dir}
+	if loc.X < m.grid.MinX || loc.X > m.grid.MaxX || loc.Y < m.grid.MinY || loc.Y > m.grid.MaxY {
+
+		return map[utils.Point]int{}
+	}
+
+	moves := beams
+	moves[loc]++
+	if _, ok := m.cache[mv]; ok {
+		return map[utils.Point]int{}
+	} else {
+		m.cache[mv] = moves
+
+		cell := m.grid.Data[loc]
+		newLoc := loc.Add(dir)
+
+		if cell == '.' {
+			m.FindBeams(newLoc, dir, moves)
+		} else if cell == '|' {
+
+			if dir == RIGHT || dir == LEFT {
+				m.FindBeams(loc.Up(), UP, moves)
+				m.FindBeams(loc.Down(), DOWN, moves)
+			} else {
+				m.FindBeams(newLoc, dir, moves)
+			}
+		} else if cell == '-' {
+			if dir == UP || dir == DOWN {
+				m.FindBeams(loc.Left(), LEFT, moves)
+				m.FindBeams(loc.Right(), RIGHT, moves)
+			} else {
+				m.FindBeams(newLoc, dir, moves)
+			}
+		} else if cell == '/' {
+			if dir == UP {
+				m.FindBeams(loc.Right(), RIGHT, moves)
+			}
+			if dir == DOWN {
+				m.FindBeams(loc.Left(), LEFT, moves)
+			}
+			if dir == LEFT {
+				m.FindBeams(loc.Down(), DOWN, moves)
+			}
+			if dir == RIGHT {
+				m.FindBeams(loc.Up(), UP, moves)
+			}
+		} else if cell == '\\' {
+			if dir == DOWN {
+				m.FindBeams(loc.Right(), RIGHT, moves)
+			}
+			if dir == UP {
+				m.FindBeams(loc.Left(), LEFT, moves)
+			}
+			if dir == LEFT {
+				m.FindBeams(loc.Up(), UP, moves)
+			}
+			if dir == RIGHT {
+				m.FindBeams(loc.Down(), DOWN, moves)
+			}
+		}
+	}
+
+	// for k, v := range moves {
+	// 	beams[k] = v
+	// }
+
+	return beams
+	// return len(beams)
+}
+
+
 func TestPartOne(t *testing.T) {
-	got := FindEnergyLevel(utils.MakeGrid(TEST_CASE), utils.Point{X: 0, Y: 0}, RIGHT, make(map[utils.Point]int), map[move]int{})
+	maze := MirrorMaze{grid: utils.MakeGrid(TEST_CASE), cache: make(map[move]map[utils.Point]int)}
+	beams := maze.FindBeams(utils.Point{X: 0, Y: 0}, RIGHT, make(map[utils.Point]int))
+	got := len(beams)
 
 	if got != 46 {
 		t.Error("Wrong answer", got)
 	} else {
-		got = FindEnergyLevel(utils.MakeGrid(input), utils.Point{X: 0, Y: 0}, RIGHT, make(map[utils.Point]int), map[move]int{})
+		maze = MirrorMaze{grid: utils.MakeGrid(input), cache: make(map[move]map[utils.Point]int)}
+		beams = maze.FindBeams(utils.Point{X: 0, Y: 0}, RIGHT, make(map[utils.Point]int))
+		got = len(beams)
 		fmt.Println(got)
 	}
 }
 
 func PartTwo(input string) int {
 	grid := utils.MakeGrid(input)
+	maze := MirrorMaze{
+		grid:  grid,
+		cache: make(map[move]map[utils.Point]int),
+	}
 	max := 0
 
 	for x := grid.MinX; x <= grid.MaxX; x++ {
-		ans := FindEnergyLevel(grid, utils.Point{X: x, Y: grid.MinY}, DOWN, make(map[utils.Point]int), make(map[move]int))
+		beams := maze.FindBeams(utils.Point{X: x, Y: grid.MinY}, DOWN, make(map[utils.Point]int))
+		ans := len(beams)
+		maze.cache = map[move]map[utils.Point]int{}
 		if ans > max {
 			max = ans
 		}
-		ans = FindEnergyLevel(grid, utils.Point{X: x, Y: grid.MaxY}, UP, make(map[utils.Point]int), make(map[move]int))
+		beams = maze.FindBeams(utils.Point{X: x, Y: grid.MaxY}, UP, make(map[utils.Point]int))
+		maze.cache = map[move]map[utils.Point]int{}
+		ans = len(beams)
 		if ans > max {
 			max = ans
 		}
 	}
 	for y := grid.MinY; y <= grid.MaxY; y++ {
-		ans := FindEnergyLevel(grid, utils.Point{X: grid.MinX, Y: y}, RIGHT, make(map[utils.Point]int), make(map[move]int))
+		beams := maze.FindBeams(utils.Point{X: grid.MinX, Y: y}, RIGHT, make(map[utils.Point]int))
+		ans := len(beams)
+		maze.cache = map[move]map[utils.Point]int{}
 		if ans > max {
 			max = ans
 		}
-		ans = FindEnergyLevel(grid, utils.Point{X: grid.MaxX, Y: y}, LEFT, make(map[utils.Point]int), make(map[move]int))
+		beams = maze.FindBeams(utils.Point{X: grid.MaxX, Y: y}, LEFT, make(map[utils.Point]int))
+		ans = len(beams)
+		maze.cache = map[move]map[utils.Point]int{}
 		if ans > max {
 			max = ans
 		}
