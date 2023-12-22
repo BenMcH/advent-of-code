e018397da07e7bcbc5519308ff4161701682da71
Ben McHone <ben@mchone.dev>
2023-12-22 00:58:16 +0000

    Better document part 2 quadratic interpolation

--- 2023/day_21/main_test.go
+++ 2023/day_21/main_test.go
@@ -120,11 +120,16 @@ func partTwo(input string, distance int) int {
 	maxI := 3
 	points := make([]int, maxI)
 	n := 65
+	// Calculate the first 3 values of the periodic repeating function. S is in the center of the grid, so the period is 65 + 131x
 	for i := 0; i < maxI; i++ {
+		// grid is 0 indexed, so add 1 to maxX when multiplying. Grid is an odd number, so we don't need to add 1 for the division
 		n = (grid.MaxX / 2) + (grid.MaxX+1)*i
-		points[i], _ = walk(grid, []utils.Point{pt}, n, true)
+		points[i], _ = walk(grid, []utils.Point{pt}, n, true) // Store how many garden plots could be occupied at time n
 	}
 
+	// Effectively storing the first derivative for each period.
+	// A second derivative shows that the acceleration between periods is constant.
+	// The second derivative is not needed for the final solution, so it is omitted.
 	deltas := make([]int, len(points)-1)
 	for i := 0; i < len(points)-1; i++ {
 		deltas[i] = points[i+1] - points[i]
@@ -134,25 +139,25 @@ func partTwo(input string, distance int) int {
 
 	// f(x)= ax^2+bx+c
 
+	// f(0) = 0a + 0b + c
+	// f(0) = c
 	c := points[0]
-
-	f1 := points[1]
-
-	f2 := points[2]
 	// a + b = f1 - c
-
 	// f(1) = a + b + c
+	f1 := points[1]
 	// f(1) - c = a + b
 	aPlusB := f1 - c
 
 	// f(2) = 4a + 2b + c
+	f2 := points[2]
 	// f(2) - c = 4a + 2b
-	// (f(2) - c)/2 = 2a + b
 	fourAPlusTwoB := f2 - c
+	// (f(2) - c)/2 = 2a + b
 	twoAPlusB := fourAPlusTwoB / 2
 
 	a, b := 1, 1
 
+	// Find integer coefficients that satisfy a + b and 2a + b values
 	for a < aPlusB {
 		b = aPlusB - a
 		ans := 2*a + b
