6f46125ee962c6a779da0f002bbaa737c3f71b74
Ben McHone <ben@mchone.dev>
2024-12-01 20:51:47 -0600

    Update template test to provide correct setups

--- 2024/resources/template_test.rb
+++ 2024/resources/template_test.rb
@@ -3,15 +3,16 @@ require 'minitest/autorun'
 require_relative './day_ZPDAY'
 require_relative './helpers'
 
-@test_input = <<~INPUT
-INPUT
-
-@test_input = @test_input.strip
-
-@input = AdventOfCodeHelpers.get_input(DAY)
-
-
 class DayDAYTest < Minitest::Test
+  def setup
+    @test_input = <<~INPUT
+    INPUT
+
+    @test_input = @test_input.strip
+
+    @input = AdventOfCodeHelpers.get_input(DAY)
+  end
+
   def test_part_1
     assert_equal 1, DayZPDAY.part_1(@test_input)
     
@@ -19,7 +20,6 @@ class DayDAYTest < Minitest::Test
   end
 
   def test_part_2
-    skip("Not implemented")
     assert_equal 1, DayZPDAY.part_2(@test_input)
 
     puts "Part 2: #{DayZPDAY.part_2(@input)}"
