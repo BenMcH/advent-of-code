e9fba9b90c0403fff75acf76562e349d37b27c51
Ben McHone <ben@mchone.dev>
2024-12-01 20:50:30 -0600

    AOC 2024 Day 1

--- 2024/lib/day_01_test.rb
+++ 2024/lib/day_01_test.rb
@@ -3,24 +3,29 @@ require 'minitest/autorun'
 require_relative './day_01'
 require_relative './helpers'
 
-@test_input = <<~INPUT
-INPUT
-
-@test_input = @test_input.strip
-
-@input = AdventOfCodeHelpers.get_input(1)
-
-
 class Day1Test < Minitest::Test
-  def test_part_1
-    assert_equal 1, Day01.part_1(@test_input)
+  def setup
+    @test_input = <<-INPUT
+      3   4
+      4   3
+      2   5
+      1   3
+      3   9
+      3   3
+    INPUT
     
+    @test_input = @test_input.strip
+    
+    @input = AdventOfCodeHelpers.get_input(1)
+  end
+
+  def test_part_1
+    assert_equal 11, Day01.part_1(@test_input)
     puts "Part 1: #{Day01.part_1(@input)}"
   end
 
   def test_part_2
-    skip("Not implemented")
-    assert_equal 1, Day01.part_2(@test_input)
+    assert_equal 31, Day01.part_2(@test_input)
 
     puts "Part 2: #{Day01.part_2(@input)}"
   end
