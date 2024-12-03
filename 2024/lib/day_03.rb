5ca5216e669e46e4b0d8747e91586302f62e672d
Ben McHone <bmchone@sourceallies.com>
2024-12-03 13:35:43 -0600

    Day 3

---
+++ 2024/lib/day_03.rb
@@ -0,0 +1,43 @@
+class Day03
+
+  def self.part_1(input)
+    # mul(n,n)
+    multipliciations = input.scan(/mul\(\d+,\d+\)/)
+    
+    products = multipliciations.map do |str|
+      a, b = str.scan(/\d+/)
+      
+      a.to_i * b.to_i
+    end
+
+    return products.sum
+  end
+  
+  def self.part_2(input)
+    instructions = input.scan(/mul\(\d+,\d+\)|do\(\)|don't\(\)/)
+    enabled = true
+    
+    products = []
+
+    instructions.each do |instruction|
+      if instruction == "do()" then
+        enabled = true
+        next
+      end
+      
+      if instruction  == "don't()" then
+        enabled = false
+        next
+      end
+      
+      unless enabled then
+        next
+      end
+
+      a, b = instruction.scan(/\d+/)
+      products << a.to_i * b.to_i
+    end
+
+    return products.sum
+  end
+end
---
+++ 2024/lib/day_03_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+
+require_relative './day_03'
+require_relative './helpers'
+
+class Day3Test < Minitest::Test
+  def setup
+    @test_input = <<~INPUT
+    xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+    INPUT
+
+    @test_input = @test_input.strip
+
+    @input = AdventOfCodeHelpers.get_input(3)
+  end
+
+  def test_part_1
+    assert_equal 161, Day03.part_1(@test_input)
+    
+    puts "Part 1: #{Day03.part_1(@input)}"
+  end
+
+  def test_part_2
+    assert_equal 48, Day03.part_2("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+
+    puts "Part 2: #{Day03.part_2(@input)}"
+  end
+end
