29e99a7af71929ec94a2c562e66228de6fabbda8
Ben McHone <ben@mchone.dev>
2024-12-07 15:30:23 -0600

    Further optimization of day 6. 15 seconds -> 2 seconds

--- 2024/lib/day_06.rb
+++ 2024/lib/day_06.rb
@@ -27,46 +27,46 @@ class Day06
   def self.part_1(input)
     grid, start = parse(input)
 
-    visited, reason = walk(grid, start)
+    visited, _ = walk(grid, start)
 
     return visited.uniq.count
   end
 
-  def self.walk(grid, start)
+  def self.walk(grid, start, part_2 = false, guard_dir = [-1, 0], states = {})
     guard_row = start[0]
     guard_col = start[1]
-    guard_dir = [-1, 0]
 
     visited = []
-    states = {}
+    count = 0
 
     row_length = grid.length
     col_length = grid[0].length
 
-    while true
+    loop do
       row = guard_row + guard_dir[0]
       col = guard_col + guard_dir[1]
+      visited_key = row * 1000 + col
 
-      if row < 0 || col < 0 || row >= row_length || col >= col_length
-        return visited, "LEFT"
-      end
+      out_of_bounds = row < 0 || col < 0 || row >= row_length || col >= col_length
+      return visited, count if out_of_bounds
 
       if grid[row][col] == "#"
         guard_dir = turn(guard_dir)
         next
       end
 
-      guard_row = row
-      guard_col = col
-      visited.push "#{row},#{col}"
-
-      key = row * 100000 + col * 100 + guard_dir[0] * 10 + guard_dir[1]
-
-      # state_key = "#{row},#{col},#{guard_dir[0]},#{guard_dir[1]}"
-      if states[key]
-        return visited, "LOOP"
+      if part_2 and not visited.include? visited_key
+        grid[row][col] = "#"
+        count += walk(grid, [guard_row, guard_col], false, guard_dir.dup, states.dup).last
+        grid[row][col] = "."
       end
 
+      guard_row = row
+      guard_col = col
+      visited.push visited_key
+
+      key = row * 100000 + col * 100 + guard_dir[0] * 10 + guard_dir[1]
+      return visited, 1 if states[key]
       states[key] = true
     end
   end
@@ -74,21 +74,6 @@ class Day06
   def self.part_2(input)
     grid, start = parse(input)
 
-    positions, _ = walk(grid, start)
-    positions = positions.uniq.map { |pos| pos.split(",").map(&:to_i) }
-
-    positions.count do |pos|
-      i, j = pos
-
-      next if i == start[0] && j == start[1]
-
-      grid[i][j] = "#"
-
-      _, reason = walk(grid, start)
-
-      grid[i][j] = "."
-
-      reason == "LOOP"
-    end
+    walk(grid, start, true).last
   end
 end
