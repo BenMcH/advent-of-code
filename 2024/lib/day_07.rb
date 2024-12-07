60068a492eab8a49ceda84ba34a9c4430733b29f
Ben McHone <ben@mchone.dev>
2024-12-07 14:24:45 -0600

    Major time savings by reversing operations

--- 2024/lib/day_07.rb
+++ 2024/lib/day_07.rb
@@ -8,35 +8,45 @@ class Day07
   end
 
   def self.can_solve(target, numbers, part_2 = false)
-    if numbers.length == 1
-      return numbers[0] == target
+    if numbers.length == 0
+      return target == 0
     end
 
-    first, second, *rest = numbers
+    first, *rest = numbers
 
-    return false if first > target
+    return false if first > target || target < 0
 
-    nums = [
-      first + second,
-      first * second,
-    ]
+    nums = []
+
+    if target >= first
+      return true if can_solve(target - first, rest, part_2)
+    end
+
+    if target % first == 0
+      return true if can_solve(target / first, rest, part_2)
+    end
 
     if part_2
-      nums << "#{first}#{second}".to_i
+      t_str = target.to_s
+
+      if t_str.end_with?(first.to_s)
+        t_str = t_str.delete_suffix(first.to_s)
+        return true if can_solve(t_str.to_i, rest, part_2)
+      end
     end
 
-    return nums.any? { |num| can_solve(target, [num] + rest, part_2) }
+    return false
   end
 
   def self.part_1(input)
     input = parse input
 
-    return input.filter { |line| can_solve(line[0], line[1..]) }.map(&:first).sum
+    return input.filter { |line| can_solve(line[0], line[1..].reverse) }.map(&:first).sum
   end
 
   def self.part_2(input)
     input = parse input
 
-    return input.filter { |line| can_solve(line[0], line[1..], true) }.map(&:first).sum
+    return input.filter { |line| can_solve(line[0], line[1..].reverse, true) }.map(&:first).sum
   end
 end
