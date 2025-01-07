71f7979e9604af21a9d5d603a88998e015879485
Ben McHone <ben@mchone.dev>
2025-01-06 21:12:48 -0600

    2018 Day 9 cleanup

--- 2018/ruby/lib/day_09.rb
+++ 2018/ruby/lib/day_09.rb
@@ -15,30 +15,23 @@ class Day09
     players, marbles = input.scan(/\d+/).map(&:to_i)
 
     game = Node.new(0)
-    zero = game
     game.left = game
     game.right = game
 
     scores = [0] * players
-    curr_index = 0
-    curr_player = 0
-
-    <<-INPUT
-      However, if the marble that is about to be placed has a number which is a multiple of 23, something entirely different happens. First, the current player keeps the marble they would have placed, adding it to their score. In addition, the marble 7 marbles counter-clockwise from the current marble is removed from the circle and also added to the current player's score. The marble located immediately clockwise of the marble that was removed becomes the new current marble.
-    INPUT
 
     (1..marbles).each do |marble|
       if marble % 23 == 0
-        scores[curr_player] += marble        
+        curr_player = marble % scores.length
 
         7.times do
           game = game.left
         end
-        left = game.left
         right = game.right
+        left = game.left
         left.right = right
         right.left = left
-        scores[curr_player] += game.val
+        scores[curr_player] += marble + game.val
         game = right
       else
         game = game.right
@@ -47,13 +40,9 @@ class Day09
         game.right.left = new_node
         game.right = new_node
 
-        game = game.right
-
+        game = new_node
       end
-
-      curr_player = (curr_player + 1) % scores.length
     end
-    
     scores.max
   end
   
