a751cc1505bd169765db9729661a524ae65297a0
Ben McHone <ben@mchone.dev>
2025-01-17 22:38:11 -0600

    Intcode p2

--- 2019/day_07/main.rb
+++ 2019/day_07/main.rb
@@ -8,22 +8,47 @@ def circuit(input, phases)
 
   5.times do |i|
     computer = Intcode.new(input)
+
+    if i > 0
+      computer.inputs = computers[i - 1].outputs
+    end
+
     computer.inputs << phases[i]
-    computer.inputs << _input
-
-    computer.step until computer.halted
-
-    _input = computer.outputs[0]
+    computers << computer
   end
 
-  _input
+  computers[0].inputs << 0
+
+  if 5 in phases
+    computers.last.outputs = computers.first.inputs
+  end
+
+  loop do
+    computers.each do |computer|
+      computer.step
+    end
+
+    if computers.last.halted
+      return computers.last.outputs.last
+    end
+  end
 end
 
 max = 0
 
-# p circuit("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", [4, 3, 2, 1, 0])
-
 (0..4).to_a.permutation.each do |phases|
+  res = circuit(input, phases)
+
+  if res > max
+    max = res
+  end
+end
+
+p max
+
+max = 0
+
+(5..9).to_a.permutation.each do |phases|
   res = circuit(input, phases)
 
   if res > max
