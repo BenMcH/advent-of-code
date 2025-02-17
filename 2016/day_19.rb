75391ca6363aa0097ea16bd8625bd32bd526cec7
Ben McHone <ben@mchone.dev>
2025-02-16 22:14:16 -0600

    Solve both parts simultaneously

--- 2016/day_19.rb
+++ 2016/day_19.rb
@@ -8,60 +8,41 @@ LinkedList = Struct.new(:elf, :next_elf) do
 	def steal!
 		self.next_elf = self.next_elf.next_elf
 	end
-	
-	def steal_across!(num_elves)
-		dist = num_elves / 2
-
-		elf = self
-		(dist - 1).times { elf = elf.next_elf }
-
-		elf.steal!
-	end
 end
 
 first = LinkedList.new(1, nil)
-
 last = first
+
+first_p2 = LinkedList.new(1, nil)
+last_p2 = first_p2
+mid_p2 = first_p2
+
 
 (2..num_elves).each do |i|
 	last.next_elf = LinkedList.new(i, nil)
 	last = last.next_elf
+
+	last_p2.next_elf = LinkedList.new(i, nil)
+	last_p2 = last_p2.next_elf
+	mid_p2 = last_p2 if i == num_elves / 2
 end
 
 last.next_elf = first
+last_p2.next_elf = first_p2
 
 elf = first
+elf_p2 = first_p2
 
 until elf.next_elf == elf
 	elf.steal!
-
-	elf = elf.next_elf
-end
-
-puts elf
-
-first = LinkedList.new(1, nil)
-last = first
-mid = first
-
-(2..num_elves).each do |i|
-	last.next_elf = LinkedList.new(i, nil)
-	last = last.next_elf
-	
-	mid = last if i == num_elves / 2
-end
-
-last.next_elf = first
-
-elf = first
-
-until elf.next_elf == elf
-	mid.steal!
+	mid_p2.steal!
 
 	num_elves -= 1
+	mid_p2 = mid_p2.next_elf if num_elves % 2 == 0
 
-	mid = mid.next_elf if num_elves % 2 == 0
 	elf = elf.next_elf
+	elf_p2 = elf_p2.next_elf
 end
 
 puts elf
+puts elf_p2
