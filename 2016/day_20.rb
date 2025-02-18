09cc1627c6cbbf0b9768a06a7d2fc70227ec67a4
Ben McHone <ben@mchone.dev>
2025-02-17 21:58:39 -0600

    p2

--- 2016/day_20.rb
+++ 2016/day_20.rb
@@ -10,11 +10,23 @@ ranges.sort_by! { |r| r.begin }
 
 n = 0
 
-enc = ranges[0]
+max = 2**32 - 1
+count = 0
+ips = []
 
-while enc
-	n = enc.end + 1
+loop do
+	break if n > max
+
 	enc = ranges.find { |r| r.include? n }
+	
+	if enc.nil?
+		ips << n
+		n += 1
+		next
+	end
+
+	n = enc.end + 1
 end
 
-p n
+p ips[0]
+p ips.length
