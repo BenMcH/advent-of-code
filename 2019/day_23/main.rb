08a6536d1c3cf45c21dc04fc61d6412479e5eafe
Ben McHone <ben@mchone.dev>
2025-04-02 21:07:23 -0500

    2019 day 23 part 2

--- 2019/day_23/main.rb
+++ 2019/day_23/main.rb
@@ -11,32 +11,57 @@ computers = computers.map.with_index do |c, i|
   c
 end
 
-wrong_address = false
+part_1 = false
+part_2 = false
+nat = nil
 
-until wrong_address do 
+idle_count = 0
+
+loop do 
+  idle = false
+
   computers.each do |c|
-    c.step
-    
-    if c.waiting_for_input
-      c.inputs << -1
-      c.step
-    end
-    
-    if c.outputs.length == 3
-      address, x, y = c.outputs
-      c.outputs = []
-      
+
+    while c.outputs.length >= 3
+      address, x, y = c.outputs.shift(3)
+
       if address == 255
-        puts y
-        wrong_address = true
-        break
+        new_nat = [x, y]
+        puts "Part 1: #{y}" unless part_1
+        part_1 = true
+
+        if nat == new_nat
+          puts "Part 2: #{y}" unless part_2
+          part_2 = true
+          break
+        end
+
+        nat = new_nat
+        next
       end
-      
+
       comp = computers[address]
       comp.inputs << x
       comp.inputs << y
     end
+
+    c.inputs << -1 if c.inputs.empty?
   end
+
+  computers.each do |c|
+    c.step
+    c.step until c.waiting_for_input
+  end
+
+  idle = computers.all?{|c| c.outputs.empty?}
+
+
+  idle_count = idle ? idle_count + 1 : 0
+  if idle_count > 1
+    computers[0].inputs << nat[0]
+    computers[0].inputs << nat[1]
+  end
+
+  break if part_2
 end
 
-computers.each(&:reset)
