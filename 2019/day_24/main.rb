00dfe1d93ddaaaf7ed0a1b17717bdd8584dac8d4
Ben McHone <ben@mchone.dev>
2025-04-02 21:35:21 -0500

    2019 day 24 part 2

--- 2019/day_24/main.rb
+++ 2019/day_24/main.rb
@@ -37,6 +37,8 @@ def next_gen(board)
       end
     end
   end
+
+  new_board
 end
 
 boards = Set.new
@@ -58,13 +60,129 @@ def bio_diversity(board)
   sum
 end
 
-loop do
-  input = next_gen(input)
-  str = str_board(input)
+# Part 1 solution
+def part1(input)
+  current_board = input.map(&:dup)
+  boards = Set.new
+  boards << str_board(current_board)
 
-  break if boards.include? str
+  loop do
+    current_board = next_gen(current_board)
+    str = str_board(current_board)
 
-  boards << str
+    break if boards.include? str
+
+    boards << str
+  end
+
+  bio_diversity(current_board)
 end
 
-p bio_diversity(input)
+puts "Part 1: #{part1(input)}"
+
+# Part 2 solution
+def recursive_neighbors(x, y, level)
+  neighbors = []
+  
+  if x == 0
+    neighbors << [1, 2, level - 1]
+  elsif x == 3 && y == 2
+    (0..4).each do |inner_y|
+      neighbors << [4, inner_y, level + 1]
+    end
+  else
+    neighbors << [x - 1, y, level]
+  end
+
+  if x == 4
+    neighbors << [3, 2, level - 1]
+  elsif x == 1 && y == 2
+    (0..4).each do |inner_y|
+      neighbors << [0, inner_y, level + 1]
+    end
+  else
+    neighbors << [x + 1, y, level]
+  end
+
+  if y == 0
+    neighbors << [2, 1, level - 1]
+  elsif x == 2 && y == 3
+    (0..4).each do |inner_x|
+      neighbors << [inner_x, 4, level + 1]
+    end
+  else
+    neighbors << [x, y - 1, level]
+  end
+
+  if y == 4
+    neighbors << [2, 3, level - 1]
+  elsif x == 2 && y == 1
+    (0..4).each do |inner_x|
+      neighbors << [inner_x, 0, level + 1]
+    end
+  else
+    neighbors << [x, y + 1, level]
+  end
+
+  neighbors
+end
+
+def count_bugs(grids)
+  grids.values.sum do |grid|
+    grid.sum { |row| row.count(@bug) }
+  end
+end
+
+def next_gen_recursive(grids)
+  min_level = grids.keys.min - 1
+  max_level = grids.keys.max + 1
+  
+  new_grids = {}
+  
+  empty_grid = Array.new(5) { Array.new(5, @empty) }
+  
+  (min_level..max_level).each do |level|
+    new_grids[level] = Array.new(5) { Array.new(5, @empty) }
+    
+    current_grid = grids[level] || empty_grid
+    
+    5.times do |i|
+      5.times do |j|
+        next if i == 2 && j == 2 # Skip the center
+        
+        bug_count = 0
+        recursive_neighbors(i, j, level).each do |nx, ny, nl|
+          # Skip if outside grid bounds or center cell
+          next if nx < 0 || nx > 4 || ny < 0 || ny > 4
+          next if nx == 2 && ny == 2
+          
+          neighbor_grid = grids[nl] || empty_grid
+          bug_count += 1 if neighbor_grid[nx][ny] == @bug
+        end
+        
+        if current_grid[i][j] == @bug
+          new_grids[level][i][j] = (bug_count == 1) ? @bug : @empty
+        else
+          new_grids[level][i][j] = (bug_count == 1 || bug_count == 2) ? @bug : @empty
+        end
+      end
+    end
+  end
+  
+  new_grids.delete(min_level) if new_grids[min_level].flatten.all? { |cell| cell == @empty }
+  new_grids.delete(max_level) if new_grids[max_level].flatten.all? { |cell| cell == @empty }
+  
+  new_grids
+end
+
+def part2(input, minutes = 200)
+  grids = { 0 => input.map(&:dup) }
+  
+  minutes.times do |i|
+    grids = next_gen_recursive(grids)
+  end
+  
+  count_bugs(grids)
+end
+
+puts "Part 2: #{part2(input)}"
