6c0bb2534930a3a213a730443b5861b1470cbace
Ben McHone <ben@mchone.dev>
2025-04-06 21:52:27 -0500

    2021 day 22 p1

--- 2021/day_22/main.rb
+++ 2021/day_22/main.rb
@@ -1,61 +1,29 @@
-InputLine = Struct.new(
-	:type,
-	:x_start,
-	:x_end,
-	:y_start,
-	:y_end,
-	:z_start,
-	:z_end
-)
+input = File.readlines("./input.txt", chomp: true)
 
-def parse_input_line(line)
-	# on x=-20..26,y=-36..17,z=-47..7	
-	chars = line.chars
-	chars.shift
-	type = chars.shift == 'n' ? :on : :off
-	tmp = chars.shift until tmp == 'x'
+lights = Hash.new(false)
 
-	chars.shift # =
-	x_start = chars.take_while{|x| '.' != x}.join.to_i
-	chars = chars[1..-1] until chars[0] == '.'
-	chars.shift # .
-	chars.shift # .
-	x_end = chars.take_while{|x| ',' != x}.join.to_i
-	chars = chars[1..-1] until chars[0] == '='
-	chars.shift # =
-	y_start = chars.take_while{|x| '.' != x}.join.to_i
-	chars = chars[1..-1] until chars[0] == '.'
-	chars.shift # .
-	chars.shift # .
-	y_end = chars.take_while{|x| ',' != x}.join.to_i
-	chars = chars[1..-1] until chars[0] == '='
-	chars.shift # =
-	z_start = chars.take_while{|x| '.' != x}.join.to_i
-	chars = chars[1..-1] until chars[0] == '.'
-	chars.shift # .
-	chars.shift # .
-	z_end = chars.join.to_i
+input.each do |line|
+	power = line.start_with? "on"
+	x_min, x_max, y_min, y_max, z_min, z_max = line.scan(/-?\d+/).map(&:to_i)
 
-	x_start, x_end = x_end, x_start if x_start > x_end
-	y_start, y_end = y_end, y_start if y_start > y_end
-	z_start, z_end = z_end, z_start if z_start > z_end
+	x_min = [x_min, -50].max
+	x_max = [x_max, 50].min
+	y_min = [y_min, -50].max
+	y_max = [y_max, 50].min
+	z_min = [z_min, -50].max
+	z_max = [z_max, 50].min
 
-	return InputLine.new(type, x_start, x_end, y_start, y_end, z_start, z_end)
-end
+	next if x_min > x_max
+	next if y_min > y_max
+	next if z_min > z_max
 
-example_input = File.read('example.txt').strip.split("\n")
-
-hash = {}
-# TODO: Implement overlapping cube detection and counting of on and off cells with map
-example_input.each do |line|
-	input = parse_input_line(line)
-	(input.x_start..input.x_end).each do |x|
-		(input.y_start..input.y_end).each do |y|
-			(input.z_start..input.z_end).each do |z|
-				hash[[x,y,z]] = input.type == :on ? 1 : 0
+	(x_min..x_max).each do |x|
+		(y_min..y_max).each do |y|
+			(z_min..z_max).each do |z|
+				lights[[x, y, z]] = power
 			end
 		end
 	end
 end
 
-p hash.values.count(1)
+p lights.values.count(true)
