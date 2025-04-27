e30e8af198b594ff27a4b627cb788c59240ddbcf
Ben McHone <ben@mchone.dev>
2025-04-26 21:34:36 -0500

    remove needless loop

--- 2018/ruby/lib/day_21.rb
+++ 2018/ruby/lib/day_21.rb
@@ -6,23 +6,21 @@ def part1
   seen_values = Set.new
   r4 = 0
   
+  r3 = r4 | 65536  # bori 4 65536 3
+  r4 = 4332021     # seti 4332021 4 4
+  
   loop do
-    r3 = r4 | 65536  # bori 4 65536 3
-    r4 = 4332021     # seti 4332021 4 4
+    r2 = r3 & 255    # bani 3 255 2
+    r4 = (r4 + r2) & 16777215  # addr 4 2 4, bani 4 16777215 4
+    r4 = (r4 * 65899) & 16777215  # muli 4 65899 4, bani 4 16777215 4
     
-    loop do
-      r2 = r3 & 255    # bani 3 255 2
-      r4 = (r4 + r2) & 16777215  # addr 4 2 4, bani 4 16777215 4
-      r4 = (r4 * 65899) & 16777215  # muli 4 65899 4, bani 4 16777215 4
-      
-      if 256 > r3  # gtir 256 3 2
-        # This is where the program checks if r4 == r0
-        # For part 1, we want the first value of r4
-        return r4
-      end
-      
-      r3 = r3 / 256  # Several instructions that effectively divide r3 by 256
+    if 256 > r3  # gtir 256 3 2
+      # This is where the program checks if r4 == r0
+      # For part 1, we want the first value of r4
+      return r4
     end
+    
+    r3 = r3 / 256  # Several instructions that effectively divide r3 by 256
   end
 end
 
