8a5fb4d16fe1a62bd7c0183bd1bfb88584b9c931
Ben McHone <bmchone@sourceallies.com>
2025-12-03 07:57:15 -0600

    Learned about dynamic programming

--- 2025/src/day_03.cr
+++ 2025/src/day_03.cr
@@ -1,23 +1,23 @@
 class Day03
-  def self.max_line(line : Array(Int64), depth = 2, curr = 0i64): Int64
-    return curr if depth == 0
+  def self.max_line(line : Array(Int64), depth = 2) : Int64
+    n = line.size
+    current = line.map_with_index { |digit, i| {i, digit} }
 
-    max = curr * 10
+    (depth - 1).times do
+      by_pos = Hash(Int32, Int64).new
 
-    line.each_with_index do |a, i|
-      n_line = line[i+1..-1]
-
-      next if n_line.size < depth - 1
-
-      candidate = max_line(n_line, depth - 1, curr * 10 + a)
-      if candidate > max
-        max = candidate
+      current.each do |pos, value|
+        ((pos + 1)...n).each do |next_pos|
+          new_value = value * 10 + line[next_pos]
+          by_pos[next_pos] = new_value if !by_pos.has_key?(next_pos) || new_value > by_pos[next_pos]
+        end
       end
+
+      current = by_pos.to_a
     end
 
-    max
+    current.map { |_, val| val }.max
   end
-
 
   def self.part_1(input : String) : Int64
     input.strip.split("\n").map(&.strip.split("").map(&.to_i64)).sum do |line|
