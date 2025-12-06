a7f0bc9090b57fd2f67854102973029a89e7f9fc
Ben McHone <ben@mchone.dev>
2025-12-06 14:59:08 -0600

    Cleanup of day 6

--- 2025/src/day_06.cr
+++ 2025/src/day_06.cr
@@ -1,66 +1,38 @@
 class Day06
+  def self.do_math(numbers : Array(Int64), operator : String) : Int64
+    operator == "*" ? numbers.product : numbers.sum
+  end
+
   def self.part_1(input : String) : Int64
     lines = input.split("\n").map(&.split(/\s+/).reject(&.empty?))
     
-    sum = 0i64
-    
-    lines.reject(&.empty?).transpose.each do |problem|
-    
-      *numbers, operator = problem
-      
-      case operator
-      when "*"
-        product = numbers.map(&.to_i64).reduce(1i64) do |acc, n|
-          acc * n
-        end
-        sum += product
-      when "+"
-        total = numbers.map(&.to_i64).sum
-        sum += total
-      end
+    lines.reject(&.empty?).transpose.sum do |column|
+      *numbers, operator = column
+      do_math(numbers.map(&.to_i64), operator)
     end
-
-
-    sum
   end
 
   def self.part_2(input : String) : Int64
     lines = input.split("\n").reject(&.empty?)
     
-    split_lines = [] of Int32
-    lines[-1].each_char.with_index do |char, index|
-      if char == '*' || char == '+'
-        split_lines << index - 1
-      end
-    end
+    operator_positions = lines[-1].each_char.with_index.select do |char, _|
+      char == '*' || char == '+'
+    end.map { |_, index| index - 1 }.to_a
     
-    signs = lines[-1].split(/\s+/).reject(&.empty?)
+    operators = lines[-1].split(/\s+/).reject(&.empty?)
+    number_rows = lines[0..-2]
     
-    sum = 0i64
-
-    signs.each_with_index do |sign, i|
-      end_index = if i + 1 < split_lines.size
-        split_lines[i + 1]
-      else
-        -1
-      end
-      line_segment = lines[0..-2].map do |line|
-        line[split_lines[i] + 1 .. end_index].chars
-      end
+    operators.each_with_index.sum do |operator, i|
+      start_pos = operator_positions[i] + 1
+      end_pos = operator_positions[i + 1]? || -1
       
-      nums = AdventOfCodeHelpers.rotate_ccw(line_segment).map(&.join.strip).reject(&.empty?).map(&.to_i64)
+      column_chars = number_rows.map { |line| line[start_pos..end_pos].chars }
+      numbers = AdventOfCodeHelpers.rotate_ccw(column_chars)
+        .map(&.join.strip)
+        .reject(&.empty?)
+        .map(&.to_i64)
       
-      case sign[0]
-      when '*'
-        sum += nums.reduce(1i64) do |acc, n|
-          acc * n
-        end
-      when '+'
-        sum += nums.sum
-      end
+      do_math(numbers, operator)
     end
-    
-
-    sum
   end
 end
