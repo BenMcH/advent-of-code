3c8cc646567aae16822fb697e1af83c83927124d
Ben McHone <bmchone@sourceallies.com>
2025-12-07 21:45:19 -0600

    day 2 golf

--- 2025/src/day_02.cr
+++ 2025/src/day_02.cr
@@ -1,32 +1,24 @@
 class Day02
-  def self.part_1(input : String) : Int64
-    sum = 0i64
-    ranges = input.split(",").map(&.split("-").map(&.to_i64)).map do |arr|
-      (arr[0]..arr[1]).to_a
-    end.flatten.sort.each do |num|
-      num_s = num.to_s
-      half = num_s[0..(num_s.size/2).to_i - 1]
-      num_s = "0" if num_s != "#{half}#{half}"
-
-      sum += num_s.to_i64
+  def self.get_ranges(input : String)
+    input.split(",").map do |str|
+      a, b = str.split("-").map(&.to_i64)
+      a..b
     end
+  end
 
-    sum
+  def self.part_1(input : String) : Int64
+    get_ranges(input).sum do |range|
+      range.sum do |num|
+        num.to_s.match(/^(\d+)\1$/) ? num : 0i64
+      end
+    end
   end
 
   def self.part_2(input : String) : Int64
-    sum = 0i64
-    invalid_regex = /^(\d+)\1+$/
-    ranges = input.split(",").map(&.split("-").map(&.to_i64)).map do |arr|
-      (arr[0]..arr[1]).to_a
-    end.flatten.sort.each do |num|
-      num_s = num.to_s
-      if num_s.match(invalid_regex) == nil
-        num_s = "0"
+    get_ranges(input).sum do |range|
+      range.sum do |num|
+        num.to_s.match(/^(\d+)\1+$/) ? num : 0i64
       end
-      sum += num_s.to_i64
     end
-
-    sum
   end
 end
