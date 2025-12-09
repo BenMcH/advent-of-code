8612e5afc211ca8f606c5ed25df83d711837442f
Ben McHone <bmchone@sourceallies.com>
2025-12-09 08:04:50 -0600

    Formatting

--- 2025/src/day_05.cr
+++ 2025/src/day_05.cr
@@ -1,9 +1,9 @@
 class Day05
   def self.part_1(input : String) : Int32
     fresh_ingredients = [] of Range(Int64, Int64)
-    
+
     fresh, available = input.split("\n\n", 2)
-    
+
     fresh.lines.each do |line|
       range_parts = line.split("-", 2)
       start_range = range_parts[0].to_i64
@@ -15,7 +15,7 @@ class Day05
     available.lines.each do |line|
       available_ingredients << line.to_i64
     end
-    
+
     available_ingredients.count do |ingredient|
       fresh_ingredients.any? do |range|
         range.includes?(ingredient)
@@ -25,18 +25,18 @@ class Day05
 
   def self.part_2(input : String) : Int64
     fresh_ingredients = [] of Range(Int64, Int64)
-    
+
     fresh, available = input.split("\n\n", 2)
-    
+
     fresh.lines.each do |line|
       range_parts = line.split("-", 2)
       start_range = range_parts[0].to_i64
       end_range = range_parts[1].to_i64
       fresh_ingredients << (start_range..end_range)
     end
-    
+
     fresh_ingredients.sort_by! &.first
-    
+
     current = fresh_ingredients[0]
     count = 0i64
 
@@ -48,7 +48,7 @@ class Day05
         current = ingredient
       end
     end
-   
+
     count + (current.end - current.begin + 1)
   end
 end
--- 2025/src/day_06.cr
+++ 2025/src/day_06.cr
@@ -5,7 +5,7 @@ class Day06
 
   def self.part_1(input : String) : Int64
     lines = input.split("\n").map(&.split(/\s+/).reject(&.empty?))
-    
+
     lines.reject(&.empty?).transpose.sum do |column|
       *numbers, operator = column
       do_math(numbers.map(&.to_i64), operator)
@@ -14,24 +14,24 @@ class Day06
 
   def self.part_2(input : String) : Int64
     lines = input.split("\n").reject(&.empty?)
-    
+
     operator_positions = lines[-1].each_char.with_index.select do |char, _|
       char == '*' || char == '+'
     end.map { |_, index| index - 1 }.to_a
-    
+
     operators = lines[-1].split(/\s+/).reject(&.empty?)
     number_rows = lines[0..-2]
-    
+
     operators.each_with_index.sum do |operator, i|
       start_pos = operator_positions[i] + 1
       end_pos = operator_positions[i + 1]? || -1
-      
+
       column_chars = number_rows.map { |line| line[start_pos..end_pos].chars }
       numbers = AdventOfCodeHelpers.rotate_ccw(column_chars)
         .map(&.join.strip)
         .reject(&.empty?)
         .map(&.to_i64)
-      
+
       do_math(numbers, operator)
     end
   end
