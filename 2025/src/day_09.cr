b04837df043dcdb2541e4cf96e929945cb8fc8dd
Ben McHone <bmchone@sourceallies.com>
2025-12-09 08:04:47 -0600

    Day 9 part 1 simplification

--- 2025/src/day_09.cr
+++ 2025/src/day_09.cr
@@ -1,23 +1,15 @@
 class Day09
-  def self.part_1(input : String) : Int64
-    points = input.lines.map do |line|
-      line.split(",").map(&.to_i64)
-    end
-
-    corners = points.combinations(2)
-
-    corners.map do |arr|
-      a, b = arr
-      x1, y1 = a
-      x2, y2 = b
-
-      x_width = (x1 - x2).abs + 1
-      y_height = (y1 - y2).abs + 1
-      x_width * y_height
-    end.max
+  def self.part_1(input : String)
+    input.lines.map(&.split(",").map(&.to_i64))
+      .combinations(2)
+      .reduce(0) do |acc, arr|
+        x1, y1, x2, y2 = arr.flatten
+        area = ((x1 - x2).abs + 1) * ((y1 - y2).abs + 1)
+        [area, acc].max
+      end
   end
 
-  def self.part_2(input : String) : Int32
+  def self.part_2(input : String)
     0
   end
 end
